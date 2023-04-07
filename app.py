d03776e11ca4268b11902cb0f9af77d3b279d942
Jianyang Hu <104685934+JianyangHu@users.noreply.github.com>
2023-04-07 23:04:58 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,36 +1,125 @@
 import streamlit as st
+
 import pandas as pd
-import numpy as np
+from PIL import Image
+from visualization.passing_network import draw_pitch, draw_pass_map
+import matplotlib.pyplot as plt
 
-st.title('Uber pickups in NYC')
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-            'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+data_all=pd.read_csv("中超2021_pass.csv")
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+
+def gendata(matchid,teamid):
+    data=pd.read_csv("中超2021_pass.csv")
+    data=data[(data["MATCH_ID"]==matchid) & (data["FROM_TEAM_ID"]==teamid)& (data["TO_TEAM_ID"]==teamid)][:]
+
+
+    data=data[["FROM_PERSON_NAME","FROM_PERSON_X","FROM_PERSON_Y","TO_PERSON_NAME","TO_PERSON_X","TO_PERSON_Y"]]
+    data=data.dropna()
+
     return data
+    
 
-data_load_state = st.text('Loading data...')
-data = load_data(10000)
-data_load_state.text("Done! (using st.cache_data)")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+def plot_passing_netowrk(passing_data):
+    passing_data=passing_data
+   
+    data=passing_data[["FROM_PERSON_NAME","FROM_PERSON_X","FROM_PERSON_Y","TO_PERSON_NAME"]]
+    data=data.dropna()
+    s1=passing_data[["FROM_PERSON_NAME","FROM_PERSON_X","FROM_PERSON_Y"]]
+    s2=passing_data[["TO_PERSON_NAME","TO_PERSON_X","TO_PERSON_Y"]]
+    s2.columns=["FROM_PERSON_NAME","FROM_PERSON_X","FROM_PERSON_Y"]
 
-st.subheader('Number of pickups by hour')
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
+    s=pd.concat((s1,s2),axis=0)
+    player_position=s
 
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+   
+    #######position
+    player_position=player_position.groupby("FROM_PERSON_NAME").mean()
 
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
+    player_position.index.name="player_name"
+
+    player_position.columns=["origin_pos_x","origin_pos_y"]
+    player_position["origin_pos_x"]=player_position["origin_pos_x"]/620
+    player_position["origin_pos_y"]=1-(player_position["origin_pos_y"])/420
+    
+    
+    
+    #########pass count
+    player_pass_count=s.copy()
+    player_pass_count=pd.DataFrame(player_pass_count.groupby("FROM_PERSON_NAME").count().iloc[:,1])
+    player_pass_count.index.name="player_name"
+    player_pass_count.columns=["num_passes"]
+    player_pass_count
+
+
+    player_pass_value=player_pass_count.copy()
+    player_pass_value.columns=["pass_value"]
+
+    set_name={}
+    #############pair count
+    pair_pass_count=data.copy()
+    pair_pass_count=pair_pass_count.reset_index()
+    
+
+    for i in range(len(pair_pass_count)):
+        set_name[str(pair_pass_count.loc[i,"FROM_PERSON_NAME"])+"_"+str(pair_pass_count.loc[i,"TO_PERSON_NAME"])]=0
+
+    for i in range(len(pair_pass_count)):
+        set_name[str(pair_pass_count.loc[i,"FROM_PERSON_NAME"])+"_"+str(pair_pass_count.loc[i,"TO_PERSON_NAME"])]+=1
+
+    pair_pass_count=pd.DataFrame(set_name,index=["num_passes"]).T
+
+    pair_pass_count.index.name="pair_key"
+
+    pair_pass_value=pair_pass_count.copy()
+
+    pair_pass_value.columns=["pass_value"]
+
+
+    from visualization.passing_network import draw_pitch, draw_pass_map
+    import matplotlib.pyplot as plt
+    plt.rcParams["font.sans-serif"]=["Songti SC"] #设置字体
+    plt.rcParams["axes.unicode_minus"]=False #该语句解决图像中的“-”负号的乱码问题
+    plot_title =""
+
+    plot_legend = "Location: pass origin\nSize: number of passes\nColor: number of passes"
+
+
+    ax = draw_pitch()
+    ax = draw_pass_map(ax, player_position, player_pass_count, player_pass_value,
+                pair_pass_count, pair_pass_value, plot_title, plot_legend)
+
+    plt.savefig("1.jpg")
+
+
+
+
+
+
+
+option_match = st.selectbox(
+    'Which match？',
+    set(data_all["MATCH_ID"].unique()))
+
+
+
+option_team=st.selectbox(
+"Which team?",
+set(data_all[data_all["MATCH_ID"]==option_match]["FROM_TEAM_ID"].unique())
+
+)
+
+
+if st.button('Start'):
+    plot_passing_netowrk(gendata(option_match,option_team))
+
+    image = Image.open('1.jpg')
+
+    st.image(image, caption='passing network ')
+
+    st.write('Finished')
+else:
+    st.write('Click here to generate the passing network')
+
+
